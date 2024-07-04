5658285e6631c93a2eba87c0d1dfdeb8dc6502e0
nhridi <n.hridi@a1qa.com>
2024-07-04 17:25:23 +0600

    fixed test.js

--- test/specs/test.js
+++ test/specs/test.js
@@ -3,18 +3,12 @@ import element from '../data/element.js';
 
 describe('W3Schools Iframe Tests', () => {
 
-    it('should verify the "HTML Tutorial" text inside the iframe', async () => {
-        const text = await iFramePage.getTextHtmlTutorial();
-        expect(text).toContain(element.htmlElement);
-    });
-
-    it('should click the CSS tutorial button and verify the "CSS Tutorial" text', async () => {
-        const text = await iFramePage.clickCssButtonAndCheckText();
-        expect(text).toContain(element.cssElement);
-    });
-
-    it('should verify the "HTML Iframe" text outside the iframe', async () => {
-        const text = await iFramePage.getTextOutsideIframe();
-        expect(text).toContain(element.iFrameElement);
+    it('should verify the "HTML Tutorial" text inside the iframe', 'should click the CSS tutorial button and verify the "CSS Tutorial" text', 'should verify the "HTML Iframe" text outside the iframe', async () => {
+        const htmlTutorial = await iFramePage.getTextHtmlTutorial();
+        const cssTutorial = await iFramePage.clickCssButtonAndCheckText();
+        const htmlIframeText = await iFramePage.getTextOutsideIframe();
+        expect(htmlTutorial).toContain(element.htmlElement);
+        expect(cssTutorial).toContain(element.cssElement);
+        expect(htmlIframeText).toContain(element.iFrameElement);
     });
 });
